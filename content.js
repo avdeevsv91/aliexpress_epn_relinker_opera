48e70f091ff565e7b09772064a36d1db8d0d5243
Sergey Avdeev <avdeevsv91@gmail.com>
2019-12-11 13:33:52 +0300

    version 1.2.3

---
+++ content.js
@@ -0,0 +1,89 @@
+﻿/*
+ =========================================================================
+ == AliExpress reLinker (ePN)                                           ==
+ == Automatic replacement of links to ePN DeepLink for AliExpress.      ==
+ == Author: Sergey Avdeev                                               ==
+ == E-Mail: avdeevsv91@gmail.com                                        ==
+ == URL: https://github.com/avdeevsv91/aliexpress_epn_relinker_opera    ==
+ =========================================================================
+*/
+
+// Document Object Model (DOM) is ready
+$(document).ready(function() {
+	// Replace links function
+	function replaceLinks() {
+		// Get local storage object from background.js
+		chrome.runtime.sendMessage({method: 'getLocalStorage'}, function(storage) {
+			// Get settings from local storage
+			var link = storage.link;
+			var sub = encodeURIComponent(storage.sub);
+			var separator = ((link.indexOf('?')==-1)?'?':'&');
+			// Select all <a href="http..."></a> tags
+			$('a').each(function() {
+				// Select aliexpress.com links
+				if(/aliexpress([\.]{1})com/i.test(this.hostname)) {
+					// Replace links
+					var url = encodeURIComponent(this.href);
+					$(this).attr('href', link+separator+'sub='+sub+'&to='+url);
+					// No referrer
+					if(storage.noreferrer == 'true') {
+						$(this).attr('rel', 'noreferrer');
+					}
+				}
+			});
+		});
+	}
+	// MutationObserver for replace links function
+	function replaceObserver(selector) {
+		var targets = document.querySelectorAll(selector);
+		if(targets) {
+			targets.forEach(function(target) {
+				var observer = new MutationObserver(function(mutations) {
+					mutations.forEach(function(mutation) {
+						replaceLinks();
+					});
+				});
+				observer.observe(target, {
+					attributes: true,
+					childList: true,
+					characterData: true
+				});
+				return observer;
+			});
+		}
+		return false;
+	}
+	// URL parser
+	var $_GET = {};
+	var __GET = window.location.search.substring(1).split('&');
+	for(var i=0; i<__GET.length; i++) {
+		var getVar = __GET[i].split('=');
+		$_GET[getVar[0]] = (getVar[1]==undefined?'':getVar[1]);
+	}
+	// The logic of determining the need for replacing links
+	if($_GET['aff_trace_key']) { // URL contains information about ePN
+		// Set (or update) relinker cookie
+		var cookie = {
+			url: 'https://aliexpress.com',
+			name: 'relinker',
+			value: Math.floor(new Date().getTime()/1000).toString(),
+			domain: '.aliexpress.com',
+			path: '/'
+		};
+		chrome.runtime.sendMessage({method: 'setCookie', details: cookie}, function(cookie) {
+			// Сookies successfully installed/updated
+		});
+	} else { // Initialize the replacement of links
+		// Check cookies on the availability of information about ePN
+		chrome.runtime.sendMessage({method: 'getCookie', details: {name: 'relinker', url: window.location.href}}, function(cookie) {
+			chrome.runtime.sendMessage({method: 'getLocalStorage'}, function(storage) {
+				if((cookie === null) || (storage.forced == 'true' && Math.floor(new Date().getTime()/1000)-cookie.value > storage.ttl)) {
+					// Replacement for product-list (ajax)
+					replaceObserver('ul');
+					// Replacement of links on DOM ready
+					replaceLinks();
+				}
+			});
+		});
+	}
+});
---
+++ settings.js
@@ -0,0 +1,81 @@
+﻿/*
+ =========================================================================
+ == AliExpress reLinker (ePN)                                           ==
+ == Automatic replacement of links to ePN DeepLink for AliExpress.      ==
+ == Author: Sergey Avdeev                                               ==
+ == E-Mail: avdeevsv91@gmail.com                                        ==
+ == URL: https://github.com/avdeevsv91/aliexpress_epn_relinker_opera    ==
+ =========================================================================
+*/
+
+// Document Object Model (DOM) is ready
+$(document).ready(function() {
+	// Get information from manifest
+	var manifest = chrome.runtime.getManifest();
+	document.title = chrome.i18n.getMessage('settings_title')+manifest.short_name; // i18n
+	$('#name').html(manifest.short_name);
+	$('#version').html(chrome.i18n.getMessage('version_title')+manifest.version); // i18n
+	$('#author').attr('href', manifest.homepage_url);
+	$('#author').html(manifest.author);
+	
+	// i18n locales
+	$('#saved').html(chrome.i18n.getMessage('saved_message'));
+	$('label[for=link_input]').html(chrome.i18n.getMessage('deeplink_label'));
+	$('label[for=sub_input]').html(chrome.i18n.getMessage('sub_label'));
+	$('label[for=forced_input]').html(chrome.i18n.getMessage('forced_label'));
+	$('label[for=ttl_input]').html(chrome.i18n.getMessage('ttl_label'));
+	$('label[for=noreferrer_input]').html(chrome.i18n.getMessage('noreferrer_label'));
+	$('input[type=submit]').val(chrome.i18n.getMessage('submit_value'));
+	$('#author_title').html(chrome.i18n.getMessage('author_title'));
+	
+	// Restore options from local storage
+	var restore_options = function() {
+		var name = $(this).attr('name');
+		if($(this).attr('type')=='checkbox') {
+			if(localStorage[name]=='true') {
+				$(this).attr('checked', true);
+			} else {
+				$(this).attr('checked', false);
+			}
+		} else {
+			$(this).val(localStorage[name]);
+		}
+	};
+	$('input[type!=submit]').each(restore_options);
+	$('select').each(restore_options);
+
+	// Save options to local storage
+	var save_options = function() {
+		var name = $(this).attr('name');
+		if($(this).attr('type')=='checkbox') {
+			if($(this).is(':checked')) {
+				localStorage[name] = true;
+			} else {
+				localStorage[name] = false;
+			}
+		} else {
+			localStorage[name] = $(this).val();
+		}
+	};
+	$('#save').click(function() {
+		$('input[type!=submit]').each(save_options);
+		$('select').each(save_options);
+		$('#saved').hide();
+		$('#saved').slideDown('slow');
+	});
+	
+	// Other actions
+	$('input[id=forced_input]').change(function(){ // Change forced mode checkbox state
+		if($(this).is(':checked')) {
+			$('label[for=ttl_input]').slideDown('slow');
+			$('input[id=ttl_input]').slideDown('slow');
+		} else {
+			$('label[for=ttl_input]').slideUp('slow');
+			$('input[id=ttl_input]').slideUp('slow');
+		}
+	});
+	if($('input[id=forced_input]').is(':checked')) { // Show tll settings if forced mode enable (on startup)
+		$('label[for=ttl_input]').show();
+		$('input[id=ttl_input]').show();
+	}
+});
